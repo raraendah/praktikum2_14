4afb02fc924b355f1fe6cf9c5c42bf5466077f14
raraendah <rarandh5@gmail.com>
2023-03-01 16:59:07 +0700

    project 1

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //Percobaan 1 dan 2
   int _counter = 1;
+  // Percobaan 3, Tugas, Latihan
+  // int _conuter = 0;
   String _text = "Ganjil";
 
   void _incrementCounter() {
@@ -38,38 +41,56 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
 
       // Percobaan 1 dan 2
-      // if(_counter>10){
-      //   // _counter = 1;
+      if(_counter>20){
+        // _counter = 1;
+        _counter = 0;
+      }
+
+      // Percobaan 2 - Menampilkan bilangan ganjil genap
+      if(_counter%2 == 0) {
+        _text = "Genap";
+      }else{
+        _text = "Ganjil";
+      }
+
+      // Percobaan 3 - Menampilkan bilangan ganjil
+      _text = "Ganjil: ";
+      for(int i=0; i<=_counter; i++){
+        if(i%2 != 0) {
+          _text += '${i}, ';
+        }
+      }
+
+      // Latihan
+      // if(_counter>20){
       //   _counter = 0;
       // }
-
-      // Percobaan 1
-      // if(_counter%2 == 0) {
-      //   _text = "Genap";
-      // }else{
-      //   _text = "Ganjil";
-      // }
-
-      // Percobaan 2
-      // _text = "Ganjil: ";
-      // for(int i=0; i<=_counter; i++){
-      //   if(i%2 != 0) {
-      //     _text += '${i}, ';
+      // _text = "Genap kelipatan 3 : ";
+      // for(int i=1; i<=_counter; i++){
+      //   if(i%3 == 0){
+      //     if(i%2 == 0) {
+      //       _text += '${i}, ';
+      //     }
       //   }
       // }
 
-      // Percobaan 3
-      if(_counter>20){
-        _counter = 0;
-      }
-      _text = "Genap kelipatan 3 : ";
-      for(int i=1; i<=_counter; i++){
-        if(i%3 == 0){
-          if(i%2 == 0) {
-            _text += '${i}, ';
-          }
-        }
-      }
+      //Tugas - Menampilkan bilangan prima
+      // if( _counter>50){
+      //   _counter = 1;
+      // }
+      // _text = "Bilangan Prima : ";
+      // for(int i=2; i<= _counter; i++){
+      //   if ( _counter % i == 0){
+      //     int a = 0;
+      //     for (int j=1; j<=i; j++ ){
+      //       if (i % j == 0){
+      //         a++;
+      //       }
+      //     }
+      //     if (a == 2){
+      //       _text += '${i}, ';
+      //     }
+      //   }
 
     });
   }
